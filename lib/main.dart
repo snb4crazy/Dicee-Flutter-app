920b7795bc2459ac7ebe55154ece4231ae70af61
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-08 23:49:38 -0400

    Add 2 expanded image assets with dicees

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,20 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          //flex sets the ratio to share space, default 1
+          flex: 1,
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          flex: 1,
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
+        ),
+      ],
+    );
   }
 }
